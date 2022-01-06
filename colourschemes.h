786c4085cc9bb0da7d5a45015b88b984e548a9b1
Hallam Smith <me@hcssmith.com>
2022-01-06 21:11:05 +0000

    Added colour scheme & desktopfile & autostarts

---
+++ colourschemes.h
@@ -0,0 +1,9 @@
+#define DECLARE_COLOURSCHEME(name) static const char* name[][3]
+
+DECLARE_COLOURSCHEME(black_and_white) = {
+	[SchemeNorm] = { "#ffffff", "#000000", "#000000" },
+	[SchemeSel]  = { "#000000", "#ffffff", "#ffffff" }, 
+//	[SchemeOut]  = { "#000000", "#ffffff", "#ffffff" }, 
+};
+
+
