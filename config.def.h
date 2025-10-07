021a421d6b510bb3a5394798652abaf176101852
Hallam Smith <hsmith@fedora.fritz.box>
2025-10-07 20:04:59 +0100

    patch apply

--- config.def.h
+++ config.def.h
@@ -2,6 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int barpadv	= 10;		/* bar vertical padding (from top)*/
+static const unsigned int barpadh	= 200;		/* bar vertical padding (from top)*/
+static const unsigned int barheight	= 2;		/* bar vertical padding (from top)*/
+static const unsigned int barborder	= 2;		/* bar vertical padding (from top)*/
+static const unsigned int floatbar	= 1;		/* 0 means bar won't float; float or dock the bar */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -16,6 +21,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeBar]  = { NULL, 		NULL,	   col_cyan  },
 };
 
 /* tagging */
@@ -86,6 +92,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
